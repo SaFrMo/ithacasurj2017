08ce3a0e9e3e7231c1fd2b24f7cca533a2bad45f
Sander <sander@funkhaus.us>
2017-06-30 09:39:30 -0400

    Added README, separated meeting-widget

---
+++ README.md
@@ -0,0 +1,6 @@
+This is the Wordpress theme for the [Ithaca chapter](http://ithacasurj.com) of [Showing Up for Racial Justice](http://www.showingupforracialjustice.org/), or SURJ. For transparency's sake, this theme is being published as open source software under the [GNU GPL v2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html).
+
+## About
+This theme is a child theme of [Ixion](https://wordpress.com/theme/ixion), with the following features added:
+
+* Auto-updating "Next Meeting" widget (`meeting-widget.php`)
---
+++ meeting-widget.php
@@ -0,0 +1,74 @@
+<?php
+
+    // Prep calendar widget
+    class Next_Meeting extends WP_Widget {
+
+        public function __construct() {
+            $widget_ops = array(
+                'classname' => 'next_meeting',
+                'description' => 'Show the date of the next chapter meeting, along with a link to more events',
+            );
+            parent::__construct( 'next_meeting', 'Next Meeting', $widget_ops );
+        }
+
+        /**
+         * Outputs the content of the widget
+         *
+         * @param array $args
+         * @param array $instance
+         */
+        public function widget( $args, $instance ) {
+            echo $args['before_widget'];
+
+            if ( ! empty( $instance['title'] ) ) {
+                echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+            }
+
+            ob_start(); ?>
+
+                <i>running</i>
+
+            <?php echo ob_get_clean();
+
+            echo $args['after_widget'];
+
+        }
+
+        /**
+         * Outputs the options form on admin
+         *
+         * @param array $instance The widget options
+         */
+        public function form( $instance ) {
+            $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Next Meeting', 'text_domain' );
+            ?>
+
+                <p>
+                    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label>
+                    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+                </p>
+
+            <?php
+        }
+
+        /**
+         * Processing widget options on save
+         *
+         * @param array $new_instance The new options
+         * @param array $old_instance The previous options
+         */
+        public function update( $new_instance, $old_instance ) {
+            $instance = array();
+            $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+            return $instance;
+        }
+
+    }
+
+    // Register widget
+    add_action( 'widgets_init', function(){
+        register_widget( 'Next_Meeting' );
+    });
+
+?>
