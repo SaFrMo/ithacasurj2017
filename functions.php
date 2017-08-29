105400f8fc8fdf4b85bf758409273ae5d8f9ce91
Sander Moolin <sandermoolin@gmail.com>
2017-08-29 18:55:49 -0400

    Enqueue jquery

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
     // Prep child theme
-    function my_theme_enqueue_styles() {
+    function enqueue_surj_styles() {
 
         wp_enqueue_style( 'ixion-style', get_template_directory_uri() . '/style.css' );
         wp_enqueue_style( 'child-style',
@@ -10,7 +10,13 @@
             wp_get_theme()->get('Version')
         );
     }
-    add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+    add_action( 'wp_enqueue_scripts', 'enqueue_surj_styles' );
+
+    // Prep scripts
+    function enqueue_surj_scripts() {
+        wp_enqueue_script('jquery');
+    }
+    add_action('wp_enqueue_scripts', 'enqueue_surj_scripts');
 
     // Get Next Meeting widget
     require_once('meeting-widget.php');
